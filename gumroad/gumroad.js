601f5a8015ce80e864bfe7165af15151af8fc2ef
Nivi Jayasekar <jnive05@gmail.com>
2021-06-09 16:38:07 -0700

    fixed pointer

--- gumroad/gumroad.js
+++ gumroad/gumroad.js
@@ -1,5 +1,5 @@
 (function(d,t){
   t=d.createElement('script');
-  t.setAttribute('src','http://127.0.0.1:4000/gumroad/gumroad-script.js');
+  t.setAttribute('src','https://heynivi.com/gumroad/gumroad-script.js');
   d.getElementsByTagName('head')[0].appendChild(t)
 })(document)
