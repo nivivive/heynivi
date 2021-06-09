077b4d20da072e986d996a34f0d3725d823e48bc
Nivi Jayasekar <jnive05@gmail.com>
2021-06-09 11:21:55 -0700

    fixed colors

---
+++ gumroad/Gumroad - Widgets_files/gumroad-embed.js
@@ -0,0 +1,1 @@
+(function(d,t){t=d.createElement('script');t.setAttribute('src','https://assets.gumroad.com/assets/gumroad-embed-365e44eb1c147b79e22c26f7937675469b99146d994dd2968c0b36f2d829e89d.js');d.getElementsByTagName('head')[0].appendChild(t)})(document)
---
+++ gumroad/Gumroad - Widgets_files/recaptcha-7487fa7f6b9f7eec923062288189e9e29e282eeb43dc905b7011f9e95a2804fd.js
@@ -0,0 +1,1 @@
+!function(){var e=document.createElement("script");e.setAttribute("src","https://www.google.com/recaptcha/api.js?render=explicit"),document.head.appendChild(e)}();
